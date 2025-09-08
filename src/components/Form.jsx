2d6c4355b1a5df36366b6a64a0cd0e5d39a76bcf
M Bilal <bilalsikander87@gmail.com>
2025-09-08 11:23:41 +0500

    little change

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -130,13 +130,13 @@ function Form() {
     }, [])
     return (
         <>
-        <div style={{backgroundColor:"#f7f8ffff"}}>
-            <div className='d-flex align-items-center justify-content-between p-2'>
-                <h1> </h1>
-                <h2>Add User</h2>
-                <div className='bg-dark p-1 rounded-1'><h3 className='text-white fs-5 pointer'>Login</h3></div>
+            <div style={{ backgroundColor: "#f7f8ffff" }}>
+                <div className='d-flex align-items-center justify-content-between p-2'>
+                    <h1> </h1>
+                    <h2>Add User</h2>
+                    <div className='bg-dark p-1 rounded-1'><h3 className='text-white fs-5 pointer'>Login</h3></div>
+                </div>
             </div>
-        </div>
             <div className='d-flex justify-content-center mt-4'>
                 <form id='form' className="form-content p-5 rounded-2" action="">
                     <label>First Name</label>
